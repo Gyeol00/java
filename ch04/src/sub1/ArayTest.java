35d39562700d29be5d936e37496b980c0f5c6c55
gyeol000726 <gyeol000726@gmail.com>
2025-01-06 10:50:34 +0900

    add ch04

--- ch04/src/sub1/ArayTest.java
+++ ch04/src/sub1/ArayTest.java
@@ -43,6 +43,7 @@ public class ArayTest {
 		for(String city : arr3) {
 			System.out.print(city + " ");
 		}
+		
 		// 1차원 배열 
 		int[] scores = {80, 60, 78, 62, 92}; // 배열 이름은 복수형태 s를 붙여서 네이밍
 		int total = 0;
@@ -51,9 +52,37 @@ public class ArayTest {
 			total += score;			
 		}
 		System.out.println("scores 합 : " + total);
-		// 2차원 배열
-		// 3차원 배열
 		
+		// 2차원 배열
+		int[][] arr2d = {{1,  2,  3,  4},
+				  		 {5,  6,  7,  8},
+				  		 {9, 10, 11, 12}};
+		
+		for(int a=0; a<3; a++) {
+			
+			for(int b=0; b<4; b++) {
+				
+				System.out.println("arr2d["+ a +"][" + b + "] : " + arr2d[a][b]);
+				
+			}
+		}
+		
+		// 3차원 배열 []면 []행 []열 순으러
+		int[][][] arr3d = {{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+				           {{10, 11, 12}, {13, 14, 15}, {16, 17, 18}},
+				           {{19, 20, 21}, {22, 23, 24}, {25, 26, 27}}};
+		
+		for(int a=0; a<3; a++) {
+			
+			for(int b=0; b<3; b++) {
+				
+				for(int c=0; c<3; c++) {
+					
+					System.out.printf("arr3d[%d][%d][%d] : %d\n", a, b, c, arr3d[a][b][c]);
+					
+				}
+			}
+		}
 		
 	}
 }
