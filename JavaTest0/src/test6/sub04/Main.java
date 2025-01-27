8d63fc8a05f456063cca933ba0eeb5b2d9a134fa
gyeol000726 <gyeol000726@gmail.com>
2025-01-27 17:24:28 +0900

    add all

---
+++ JavaTest0/src/test6/sub04/Main.java
@@ -0,0 +1,28 @@
+package test6.sub04;
+
+public class Main {
+	public static void main(String[] args) {
+		Book book = new Book("이것이 자바다.", "신용권", "101-1234-1001");
+		
+		Member member = new Member("홍길동", "A001");
+		
+		book.getBookInfo();
+		System.out.println("--------------------");
+		
+		member.borrowBook(book);
+		System.out.println("--------------------");
+		
+		book.getBookInfo();
+		System.out.println("--------------------");
+		
+		member.borrowBook(book);
+		System.out.println("--------------------");
+		
+		member.returnBook(book);
+		System.out.println("--------------------");
+		
+		member.getMemberInfo();
+		System.out.println("--------------------");
+		
+	}
+}
